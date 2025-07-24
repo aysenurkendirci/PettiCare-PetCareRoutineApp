4a3c94a2adf46b743d6a9c8934585db438d1867b
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-07-24 23:27:13 +0300

    Initial Commit

---
+++ PetCareAppTask3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetCareAppTask3
+//
+//  Created by Ayşe Nur Kendirci on 24.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
