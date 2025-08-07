bb0d0b4e93468550f158ba6c25907a4bec2404ce
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-08-07 15:57:55 +0300

    feat: Add new pet upload button and support viewing/adding routines for multiple pet types

---
+++ PetCareAppTask3/View/AddRoutineSheetView/RoutineFrequencyPickerView.swift
@@ -0,0 +1,8 @@
+//
+//  RoutineFrequencyPickerView.swift
+//  PetCareAppTask3
+//
+//  Created by Ayşe Nur Kendirci on 7.08.2025.
+//
+
+import Foundation
---
+++ PetCareAppTask3/View/AddRoutineSheetView/RoutineNameInputView.swift
@@ -0,0 +1,8 @@
+//
+//  RoutineNameInputView.swift
+//  PetCareAppTask3
+//
+//  Created by Ayşe Nur Kendirci on 7.08.2025.
+//
+
+import Foundation
